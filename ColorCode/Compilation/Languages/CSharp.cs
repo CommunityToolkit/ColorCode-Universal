432f20251beca108267dead7429e20be8ef98620
csano <none@none>
2011-01-26 15:09:36 -0800

    Added dynamic and yield keywords to CSharp language

--- ColorCode/Compilation/Languages/CSharp.cs
+++ ColorCode/Compilation/Languages/CSharp.cs
@@ -83,7 +83,7 @@ namespace ColorCode.Compilation.Languages
                                            { 1, ScopeName.PreprocessorKeyword }
                                        }),
                                new LanguageRule(
-                                   @"\b(abstract|as|ascending|base|bool|break|by|byte|case|catch|char|checked|class|const|continue|decimal|default|delegate|descending|do|double|else|enum|equals|event|explicit|extern|false|finally|fixed|float|for|foreach|from|get|goto|group|if|implicit|in|int|into|interface|internal|is|join|let|lock|long|namespace|new|null|object|on|operator|orderby|out|override|params|partial|private|protected|public|readonly|ref|return|sbyte|sealed|select|set|short|sizeof|stackalloc|static|string|struct|switch|this|throw|true|try|typeof|uint|ulong|unchecked|unsafe|ushort|using|var|virtual|void|volatile|where|while)\b",
+                                   @"\b(abstract|as|ascending|base|bool|break|by|byte|case|catch|char|checked|class|const|continue|decimal|default|delegate|descending|do|double|dynamic|else|enum|equals|event|explicit|extern|false|finally|fixed|float|for|foreach|from|get|goto|group|if|implicit|in|int|into|interface|internal|is|join|let|lock|long|namespace|new|null|object|on|operator|orderby|out|override|params|partial|private|protected|public|readonly|ref|return|sbyte|sealed|select|set|short|sizeof|stackalloc|static|string|struct|switch|this|throw|true|try|typeof|uint|ulong|unchecked|unsafe|ushort|using|var|virtual|void|volatile|where|while|yield)\b",
                                    new Dictionary<int, string>
                                        {
                                            { 1, ScopeName.Keyword }
