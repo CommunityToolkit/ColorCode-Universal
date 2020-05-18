08302b2b3cd42ff243795f8259edd086478827bb
Alexandre Zollinger Chohfi <alzollin@microsoft.com>
2020-05-17 18:04:24 -0700

    Added test project.

--- Tests/ColorCode.WinUITests/MainWindow.xaml.cs
+++ Tests/ColorCode.WinUITests/MainWindow.xaml.cs
@@ -10,18 +10,13 @@ using Windows.System;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
-namespace ColorCode.UWPTests
+namespace ColorCode.WinUITests
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainWindow
     {
-        public MainPage()
+        public MainWindow()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
 
         private async Task<Tuple<string, ILanguage>> GetCodeFileText()
